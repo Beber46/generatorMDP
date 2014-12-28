2fdcad611e16c552c1a63e1724c7a3167e7d30e5
Beber46 <lafage.bertrand@gmail.com>
2014-12-28 17:10:05 +0100

    ajout de la phase 1

--- app/src/main/java/fr/beber/generatormdp/WelcomeActivity.java
+++ app/src/main/java/fr/beber/generatormdp/WelcomeActivity.java
@@ -1,7 +1,11 @@
 package fr.beber.generatormdp;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
+import android.view.View;
+import android.widget.ImageView;
 
 public class WelcomeActivity extends Activity {
     /**
@@ -10,6 +14,30 @@ public class WelcomeActivity extends Activity {
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.main);
+        setContentView(R.layout.activity_welcome);
+
+        final ImageView imageViewLogo = (ImageView)findViewById(R.id.IVLogo);
+
+        imageViewLogo.setOnClickListener(this.clickListenerLogo);
+
+        final Handler handler = new Handler();
+        handler.postDelayed(new Runnable() {
+            public void run() {
+                imageViewLogo.setOnClickListener(clickListenerLogo);
+            }
+        }, 2000);
     }
+
+
+    /**
+     * Permet de passer à l'activity suivante.
+     */
+    private View.OnClickListener clickListenerLogo = new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+            final Intent intent=new Intent(WelcomeActivity.this,MainActivity.class);
+            startActivity(intent);
+            finish();
+        }
+    };
 }
